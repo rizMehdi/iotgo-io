38fb58b8365181e3ebba15fd8904c784c31ac2fc
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-08-03 10:33:37 +0200

    Update iotgo.py

--- webapp/iotgo.py
+++ webapp/iotgo.py
@@ -21,7 +21,7 @@ with persona:
 with thing:
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-thing-art-7.png", width=cardWidth)
 
-input_col, plus_col, output_col,  code_col, emptycol , emptycol2 , emptycol3, emptycol4,emptycol5,emptycol56, emptycol6 = st.beta_columns(11)
+input_col, plus_col, output_col,  code_col, emptycol , emptycol2 , emptycol3, emptycol4,emptycol5,emptycol56, emptycol561, emptycol561, emptycol6 = st.beta_columns(13)
 
 
 with input_col:    
