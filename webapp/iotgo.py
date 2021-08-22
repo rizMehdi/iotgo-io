8adc19dd663ba6b9c4fa18b50c173cc9f7af58c9
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-08-22 21:44:07 +0200

    file-updated on Sun Aug 22 20:44:07 2021

--- webapp/iotgo.py
+++ webapp/iotgo.py
@@ -1,4 +1,4 @@
-#this file was updated on Sun Aug 22 20:44:03 2021
+#this file was updated on Sun Aug 22 20:44:06 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide")
@@ -17,7 +17,7 @@ with applogo:
 with mission:
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-noMission.png", width=missionCardWidth)
 with persona:
-    st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-noPersona.png", width=cardWidth)
+    st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-persona-3.png", width=cardWidth)
 with thing:
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-thing-art-4.png", width=cardWidth)
 with edit:
