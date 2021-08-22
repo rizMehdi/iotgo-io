74d0337c7b5b3af7956fbaa9d02b8d808d730f1c
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-08-22 21:50:41 +0200

    file-updated on Sun Aug 22 20:50:40 2021

--- webapp/iotgo.py
+++ webapp/iotgo.py
@@ -1,8 +1,8 @@
-#this file was updated on Sun Aug 22 20:50:29 2021
+#this file was updated on Sun Aug 22 20:50:40 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide")
-urlis=""
+urlis="https://makecode.microbit.org/--docs?md=Not%20Enough%20cards%20to%20generate%20code"
 
 cardWidth=100
 pluscardwidht=100
@@ -46,7 +46,7 @@ with output_col:
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth)
     st.write("then...")
     # ("Output1:")
-    st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-noOutput.png", width=cardWidth)
+    st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-ouputPhy-RotateMin.png", width=cardWidth)
     # ("Output2:")
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-noOutput.png", width=cardWidth)
     # ("Output3:")
