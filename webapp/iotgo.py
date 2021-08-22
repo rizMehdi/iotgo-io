533377a184af90fcbaf238815e7e2e85173bcf6d
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-08-22 23:30:49 +0200

    file-updated on Sun Aug 22 22:30:49 2021

--- webapp/iotgo.py
+++ webapp/iotgo.py
@@ -1,4 +1,4 @@
-#this file was updated on Sun Aug 22 22:30:30 2021
+#this file was updated on Sun Aug 22 22:30:48 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide")
@@ -34,7 +34,7 @@ with input_col:
     # ("Input2:")
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-inputPhy-GestureShake.png", width=cardWidth)
     # ("Input3:")
-    st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-noInput.png", width=cardWidth)
+    st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-inputPhy-AccelerationLow.png", width=cardWidth)
 
 with plus_col:    
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth*2)
