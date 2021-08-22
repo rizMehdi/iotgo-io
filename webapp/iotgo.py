8f202c63ac2e597eb37e092c8792a6ad9f38292a
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-08-22 22:04:33 +0200

    file-updated on Sun Aug 22 21:04:33 2021

--- webapp/iotgo.py
+++ webapp/iotgo.py
@@ -1,4 +1,4 @@
-#this file was updated on Sun Aug 22 21:04:30 2021
+#this file was updated on Sun Aug 22 21:04:32 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide")
@@ -30,7 +30,7 @@ with input_col:
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth)
     st.write("if...")
     # ("Input1:")
-    st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-inputPhy-SliderMaximum.png", width=cardWidth)
+    st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-inputPhy-MovementNotPresent.png", width=cardWidth)
     # ("Input2:")
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-noInput.png", width=cardWidth)
     # ("Input3:")
