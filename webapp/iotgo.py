032f08e875b7d6998f300ae4259c2e92b3f4ebfd
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-08-22 22:12:02 +0200

    file-updated on Sun Aug 22 21:12:02 2021

--- webapp/iotgo.py
+++ webapp/iotgo.py
@@ -1,4 +1,4 @@
-#this file was updated on Sun Aug 22 21:11:48 2021
+#this file was updated on Sun Aug 22 21:12:02 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide")
@@ -32,7 +32,7 @@ with input_col:
     # ("Input1:")
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-inputCloud-ForecastWindHigh.png", width=cardWidth)
     # ("Input2:")
-    st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-noInput.png", width=cardWidth)
+    st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-inputPhy-MovementNotPresent.png", width=cardWidth)
     # ("Input3:")
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-noInput.png", width=cardWidth)
 
