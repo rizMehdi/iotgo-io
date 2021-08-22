82237245a0d9cbadc56e8c4a0e732f49ed3ceadf
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-08-23 00:07:36 +0200

    file-updated on Sun Aug 22 23:07:36 2021

--- webapp/iotgo.py
+++ webapp/iotgo.py
@@ -1,8 +1,8 @@
-#this file was updated on Sun Aug 22 23:07:32 2021
+#this file was updated on Sun Aug 22 23:07:35 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide")
-urlis="https://makecode.microbit.org/--docs?md=%20"
+urlis="https://makecode.microbit.org/--docs?md=%0A%0A%60%60%60%20blocks%0Aradio.setGroup%28313%29%0Aradio.onReceivedValue%28function%20%28name%2C%20value%29%20%7B%0A%20forecastName%20%3D%20name%0AforecastValue%20%3D%20value%0A%7D%29%0Alet%20forecastValue%20%3D%200%0Alet%20forecastName%20%3D%20%22none%22%20%0Abasic.pause%281000%29%0Abasic.forever%28function%20%28%29%20%7B%0A%20%20%20%20if%20%28forecastName%20%3D%3D%20%22wind%22%20%26%26%20forecastValue%20%3E%3D%200.5%29%7B%0A%20%20%20%20%20%20%20%20pins.digitalWritePin%28DigitalPin.P1%2C1%29%0A%20%20%20%20%20%20%20%20basic.pause%28100%29%0A%20%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20radio.sendString%28%22get_wind%22%29%0Abasic.pause%282000%29%0A%0A%20%20%20%20%7D%0A%7D%29%0A%60%60%60%0A%0A"
 
 cardWidth=100
 pluscardwidht=100
