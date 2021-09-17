488bf8012d1493b8632a2249129f2f48406e6d79
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-09-17 15:16:52 +0200

    file-updated on Fri Sep 17 14:16:51 2021

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -1,4 +1,4 @@
-#this file was updated on Fri Sep 17 14:16:48 2021
+#this file was updated on Fri Sep 17 14:16:50 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide")
