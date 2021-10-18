07729bb920f084197a8a4fa73b1e08233ca2488f
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-10-18 10:44:08 +0200

    file-updated on Mon Oct 18 09:44:07 2021

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -1,8 +1,8 @@
-#this file was updated on Mon Oct 18 09:44:05 2021
+#this file was updated on Mon Oct 18 09:44:07 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide")
-urlis="https://makecode.microbit.org/--docs?md=%20"
+urlis="https://makecode.microbit.org/--docs?md=%0A%0A%60%60%60%20blocks%0Abasic.forever%28function%20%28%29%20%7B%0A%20%20%20%20if%20%28%29%7B%0A%20%20%20%20%20%20%20%20basic.showString%28%22Ciao%20%22%29%0A%20%20%20%20%20%20%20%20basic.pause%281000%29%0A%20%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20basic.clearScreen%28%29%0Abasic.pause%28100%29%0A%0A%0A%20%20%20%20%7D%0A%20%20%20%20if%20%28%29%7B%0A%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20basic.pause%281000%29%0A%20%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20%0A%0A%20%20%20%20%7D%0A%20%20%20%20if%20%28%29%7B%0A%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20basic.pause%281000%29%0A%20%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20%0A%0A%20%20%20%20%7D%0A%7D%29%0A%60%60%60%0A%0A"
 
 cardWidth=100
 pluscardwidht=100
