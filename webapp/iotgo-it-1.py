2bc84e41b7e434dc9caeac4992f3c9fdb07985aa
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-10-18 16:36:20 +0200

    file-updated on Mon Oct 18 15:36:20 2021

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -1,8 +1,8 @@
-#this file was updated on Mon Oct 18 15:35:59 2021
+#this file was updated on Mon Oct 18 15:36:19 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide")
-urlis="https://makecode.microbit.org/--docs?md=%20"
+urlis="https://makecode.microbit.org/--docs?md=%0A%0A%60%60%60%20blocks%0Abasic.pause%281000%29%0Abasic.forever%28function%20%28%29%20%7B%0A%20%20%20%20if%20%28input.lightLevel%28%29%20%3C%20127%29%7B%0A%20%20%20%20%20%20%20%20basic.showIcon%28IconNames.Happy%29%20%0A%20basic.pause%281000%29%0A%20%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20basic.clearScreen%28%29%0Abasic.pause%281000%29%0A%0A%0A%20%20%20%20%7D%0A%7D%29%0A%60%60%60%0A%0A"
 
 cardWidth=100
 pluscardwidht=100
