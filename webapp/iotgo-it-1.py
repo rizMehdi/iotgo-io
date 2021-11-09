36f678254556f351027429d6ed1473b763a09b28
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-09 15:39:01 +0100

    Update iotgo-it-1.py

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -13,11 +13,11 @@ st.markdown(
     """
     <style>
     [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-        width: 410px;
+        width: 400px;
     }
     [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-        width: 410px;
-        margin-left: -380px;
+        width: 400px;
+        margin-left: -400px;
     }
     </style>
     """,
