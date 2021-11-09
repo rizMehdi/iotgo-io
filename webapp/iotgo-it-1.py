f157f661ddf50eb80943b31ffd609fb26b39f9a9
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-09 15:21:43 +0100

    Update iotgo-it-1.py

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -71,8 +71,8 @@ with output_col:
 
 #applogo,edit  = st.beta_columns(3,5)
 
-with code:
-    components.iframe(urlis,width=800, height=1000, scrolling=True)
+#with code:
+components.iframe(urlis,width=800, height=1000, scrolling=True)
 #with edit:
     #st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=60)
     #st.markdown("[Modifica]("+urlis+")", unsafe_allow_html=True)
