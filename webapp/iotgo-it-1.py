fb6bc4076daf5afb5e49f497a864dc92043f3cfa
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-09 14:11:27 +0100

    Update iotgo-it-1.py

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -66,6 +66,6 @@ with output_col:
 with code_col:
     # st.header("Il mio codice è:")
     #components.iframe(urlis, scrolling=True)
-    components.iframe(urlis,width=2000, height=2400, scrolling=False)
+    components.iframe(urlis,width=1200, height=1000, scrolling=True)
 
 st.button("Refresh")
