b5627f770861f2d0152b56750b77fc484b826bd3
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-10 14:03:02 +0100

    Update app.py

--- multiPageWebApp/app.py
+++ multiPageWebApp/app.py
@@ -2,11 +2,44 @@
 import app1
 import app2
 import streamlit as st
-PAGES = {
-    "App1": app1,
-    "App2": app2
+#PAGES = {
+#    "App1": app1,
+##    "App2": app2
 }
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+#st.sidebar.title('Navigation')
+#selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+#page = PAGES[selection]
+#page.app()
+
+
+def tabs(default_tabs = [], default_active_tab=0):
+        if not default_tabs:
+            return None
+        active_tab = st.radio("", default_tabs, index=default_active_tab, key='tabs')
+        child = default_tabs.index(active_tab)+1
+        st.markdown("""  
+            <style type="text/css">
+            div[role=radiogroup] > label > div:first-of-type, .stRadio > label {
+               display: none;               
+            }
+            div[role=radiogroup] {
+                flex-direction: unset
+            }
+            div[role=radiogroup] label {             
+                border: 1px solid #999;
+                background: #EEE;
+                padding: 4px 12px;
+                border-radius: 4px 4px 0 0;
+                position: relative;
+                top: 1px;
+                }
+            div[role=radiogroup] label:nth-child(""" + str(child) + """) {    
+                background: #FFF !important;
+                border-bottom: 1px solid transparent;
+            }            
+            </style>
+        """,unsafe_allow_html=True)        
+        return active_tab
+
+active_tab = tabs(["app1", "app2"])
+st.write(active_tab)
