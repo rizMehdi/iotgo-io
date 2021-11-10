8f832cb8ed027d5c65fdf2a53923656aed361057
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-10 14:04:06 +0100

    Update app.py

--- multiPageWebApp/app.py
+++ multiPageWebApp/app.py
@@ -5,7 +5,7 @@ import streamlit as st
 #PAGES = {
 #    "App1": app1,
 ##    "App2": app2
-}
+#}
 #st.sidebar.title('Navigation')
 #selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 #page = PAGES[selection]
