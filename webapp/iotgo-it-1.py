10f6a1f06bcff35c90008d3b6335e087d97aceb4
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-10 12:12:56 +0100

    file-updated on Wed Nov 10 11:12:56 2021

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -1,4 +1,4 @@
-                #this file was updated on Wed Nov 10 11:12:39 2021
+                #this file was updated on Wed Nov 10 11:12:56 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide",initial_sidebar_state="expanded")
@@ -42,25 +42,11 @@ st.markdown(
 
 
 st.sidebar.image("http://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/applogo-hor.png",width=380)
-input_col, plus_col, output_col, empty= st.sidebar.columns([1,1,1,1])
-with input_col:    
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth)
-	st.write(" se...")
-	# ("Input1:")
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-noInput.png", width=cardWidth)
-	# ("Input2:")
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-noInput.png", width=cardWidth)
-with plus_col:    
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth*2)
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/plus.png", width=pluscardwidht)
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/plus.png", width=pluscardwidht) 
-with output_col:    
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth)
-	st.write(" allora...")
-	# ("Output1:")
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-noOutput.png", width=cardWidth)
-	# ("Output2:")
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-noOutput.png", width=cardWidth) 
+st.sidebar.markdown(
+    """
+    Scansiona una carta codice per iniziare, quindi scansiona le carte di input e output // Scan a code card to start and then scan input and output cards. 
+    """
+    )
 
 
 st.subheader("")
