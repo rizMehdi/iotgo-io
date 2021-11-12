66b77e9cfd3a3644bd01fbb1ea1025ebca14151b
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-12 06:29:42 +0100

    file-updated on Fri Nov 12 05:29:42 2021

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -1,8 +1,8 @@
-                #this file was updated on Fri Nov 12 05:28:31 2021
+                #this file was updated on Fri Nov 12 05:29:42 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide",initial_sidebar_state="expanded")
-urlis="https://makecode.microbit.org/--docs?md=%0A%0A%60%60%60%20blocks%0Abasic.pause%281000%29%0Abasic.forever%28function%20%28%29%20%7B%0A%20%20%20%20if%20%28pins.digitalReadPin%28DigitalPin.P0%29%20%3D%3D%200%29%7B%0A%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20%0A%0A%20%20%20%20%7D%0A%7D%29%0A%60%60%60%0A%0A"
+urlis=""
 cardWidth=130
 pluscardwidht=130
 missionCardWidth=160
@@ -41,40 +41,24 @@ st.markdown(
 
 
 st.sidebar.image("http://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/applogo-hor.png",width=380)
-input_col, plus_col, output_col, empty= st.sidebar.columns([1,1,1,1])
-with input_col:    
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth)
-	st.write(" se...")
-	# ("Input1:")
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-inputPhy-MovementNotPresent.png", width=cardWidth) 
-with plus_col:    
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth*2)
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/plus.png", width=pluscardwidht) 
-with output_col:    
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth)
-	st.write(" allora...")
-	# ("Output1:")
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-noOutput.png", width=cardWidth)
+st.sidebar.markdown(
+    """
+    Scansiona una carta codice per iniziare, quindi scansiona le carte di input e output // Scan a code card to start and then scan input and output cards. 
+    """
+    )
 
 
 st.subheader("")
 st.markdown(
         """
         <style> .font{
-        font-size:30px;}
+        font-size:50px;}
         </style>
         """,
         unsafe_allow_html=True,
         )
-st.code('''basic.pause(1000)
-basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P0) == 0){
+st.code('''''',language="javascript")
 
-     } else {
-
-
-    }
-})''',language="javascript")
 st.subheader("")
 components.iframe(urlis, height=1000, scrolling=True)    
 
