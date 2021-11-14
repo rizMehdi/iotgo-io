a054a1367a75200129eafe2af31d8471234999cd
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-14 22:08:48 +0100

    file-updated on Sun Nov 14 21:08:47 2021

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -1,8 +1,8 @@
-                #this file was updated on Sun Nov 14 21:08:46 2021
+                #this file was updated on Sun Nov 14 21:08:47 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide",initial_sidebar_state="expanded")
-urlis="https://makecode.microbit.org/--docs?md=%0A%0A%60%60%60%20blocks%0Aradio.setGroup%281%29%0Abasic.forever%28function%20%28%29%20%7B%0A%20%20%20%20if%20%28input.lightLevel%28%29%20%3C%20127%29%7B%0A%20%20%20%20%20%20%20%20radio.sendValue%28%22noInput%22%2CnoInput%29%0A%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20basic.pause%281000%29%0A%20%20%20%20%7D%0A%7D%29%0A%60%60%60%0A%0A"
+urlis="https://makecode.microbit.org/--docs?md=%0A%0A%60%60%60%20blocks%0Aradio.setGroup%281%29%0Abasic.forever%28function%20%28%29%20%7B%0A%20%20%20%20if%20%28input.acceleration%28Dimension.X%29%20%3E%3D%20511%29%7B%0A%20%20%20%20%20%20%20%20radio.sendValue%28%22noInput%22%2CnoInput%29%0A%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20basic.pause%281000%29%0A%20%20%20%20%7D%0A%7D%29%0A%60%60%60%0A%0A"
 cardWidth=130
 pluscardwidht=130
 missionCardWidth=160
@@ -68,7 +68,7 @@ st.markdown(
         )
 st.code('''radio.setGroup(1)
 basic.forever(function () {
-    if (input.lightLevel() < 127){
+    if (input.acceleration(Dimension.X) >= 511){
         radio.sendValue("noInput",noInput)
     } else {
         basic.pause(1000)
