f117afc46028f13714ae3e137dae970b0ccac7ba
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-14 23:06:30 +0100

    file-updated on Sun Nov 14 22:06:30 2021

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -1,4 +1,4 @@
-                #this file was updated on Sun Nov 14 22:06:10 2021
+                #this file was updated on Sun Nov 14 22:06:29 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide",initial_sidebar_state="expanded")
@@ -59,7 +59,7 @@ with output_col:
 	# ("Output1:")
 	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-sendData.png", width=cardWidth)
 	# ("Output2:")
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-noOutput.png", width=cardWidth) 
+	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-outputPhy-TurnOffRainbowLight.png", width=cardWidth) 
 
 
 st.subheader("")
