d794dfc3365ea2bacefc1be116fa10d9406decb4
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-14 22:50:25 +0100

    file-updated on Sun Nov 14 21:50:25 2021

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -1,8 +1,8 @@
-                #this file was updated on Sun Nov 14 21:50:23 2021
+                #this file was updated on Sun Nov 14 21:50:25 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide",initial_sidebar_state="expanded")
-urlis="https://makecode.microbit.org/--docs?md=%0A%0A%60%60%60%20blocks%0Aradio.setGroup%281%29%0Abasic.forever%28function%20%28%29%20%7B%0A%20%20%20%20if%20%28input.compassHeading%28%29%20%3E%3D%20225%20%26%26%20input.compassHeading%28%29%20%3C%20315%29%7B%0A%20%20%20%20%20%20%20%20radio.sendValue%28%22compassW%22%2C1%29%0A%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20basic.pause%281000%29%0A%20%20%20%20%7D%0A%7D%29%0A%60%60%60%0A%0A"
+urlis="https://makecode.microbit.org/--docs?md=%0A%0A%60%60%60%20blocks%0Aradio.setGroup%281%29%0Alet%20recieved%20%3D%200%0Aradio.setGroup%281%29%0Abasic.forever%28function%20%28%29%20%7B%0A%20%20%20%20if%20%28recieved%20%3D%3D%201%29%7B%0A%20%20%20%20%20%20%20%20radio.sendValue%28%22recieveData%22%2C1%29%0A%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20basic.pause%281000%29%0A%20%20%20%20%7D%0A%7D%29%0Aradio.onReceivedValue%28function%20%28name%2C%20value%29%20%7B%0A%09if%20%28name%20%3D%3D%20%22pulsante%22%20%26%26%20value%20%3D%3D%201%29%20%7B%0A%09%09recieved%20%3D%201%0A%09%7D%0A%7D%29%0A%0A%60%60%60%0A%0A"
 cardWidth=130
 pluscardwidht=130
 missionCardWidth=160
@@ -67,13 +67,21 @@ st.markdown(
         unsafe_allow_html=True,
         )
 st.code('''radio.setGroup(1)
+let recieved = 0
+radio.setGroup(1)
 basic.forever(function () {
-    if (input.compassHeading() >= 225 && input.compassHeading() < 315){
-        radio.sendValue("compassW",1)
+    if (recieved == 1){
+        radio.sendValue("recieveData",1)
     } else {
         basic.pause(1000)
     }
-})''',language="javascript")
+})
+radio.onReceivedValue(function (name, value) {
+	if (name == "pulsante" && value == 1) {
+		recieved = 1
+	}
+})
+''',language="javascript")
 
 e,edit  = st.columns([1,1])
 with edit:
