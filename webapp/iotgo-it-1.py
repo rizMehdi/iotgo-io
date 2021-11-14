a953cab7b12f533e41fd282331d98e05b518b699
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-14 22:53:16 +0100

    file-updated on Sun Nov 14 21:53:15 2021

--- webapp/iotgo-it-1.py
+++ webapp/iotgo-it-1.py
@@ -1,8 +1,8 @@
-                #this file was updated on Sun Nov 14 21:50:46 2021
+                #this file was updated on Sun Nov 14 21:53:15 2021
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide",initial_sidebar_state="expanded")
-urlis="https://makecode.microbit.org/--docs?md=%0A%0A%60%60%60%20blocks%0Aradio.setGroup%281%29%0Alet%20recieved%20%3D%200%0Alet%20strip%20%3D%20neopixel.create%28DigitalPin.P1%2C7%2CNeoPixelMode.RGB%29%0Abasic.forever%28function%20%28%29%20%7B%0A%20%20%20%20if%20%28recieved%20%3D%3D%201%29%7B%0A%20%20%20%20%20%20%20%20strip.showColor%28neopixel.colors%28NeoPixelColors.Black%29%29%0A%09basic.pause%281000%29%0A%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20strip.showRainbow%281%2C%20360%29%0A%09basic.pause%281000%29%0A%20%20%20%20%7D%0A%7D%29%0Aradio.onReceivedValue%28function%20%28name%2C%20value%29%20%7B%0A%09if%20%28name%20%3D%3D%20%22pulsante%22%20%26%26%20value%20%3D%3D%201%29%20%7B%0A%09%09recieved%20%3D%201%0A%09%7D%0A%7D%29%0A%0A%60%60%60%0A%0A%60%60%60package%0Aneopixel%3Dgithub%3Amicrosoft%2Fpxt-neopixel%0A%0A%60%60%60"
+urlis=""
 cardWidth=130
 pluscardwidht=130
 missionCardWidth=160
@@ -41,20 +41,11 @@ st.markdown(
 
 
 st.sidebar.image("http://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/applogo-hor.png",width=380)
-input_col, plus_col, output_col, empty= st.sidebar.columns([1,1,1,1])
-with input_col:    
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth)
-	st.write(" se...")
-	# ("Input1:")
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-recieveData.png", width=cardWidth) 
-with plus_col:    
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth*2)
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/plus.png", width=pluscardwidht) 
-with output_col:    
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/blankcard.png", width=vertiPaddingWidth)
-	st.write(" allora...")
-	# ("Output1:")
-	st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/IT-outputPhy-TurnOffRainbowLight.png", width=cardWidth)
+st.sidebar.markdown(
+    """
+    Scansiona una carta codice per iniziare, quindi scansiona le carte di input e output // Scan a code card to start and then scan input and output cards. 
+    """
+    )
 
 
 st.subheader("")
@@ -66,24 +57,7 @@ st.markdown(
         """,
         unsafe_allow_html=True,
         )
-st.code('''radio.setGroup(1)
-let recieved = 0
-let strip = neopixel.create(DigitalPin.P1,7,NeoPixelMode.RGB)
-basic.forever(function () {
-    if (recieved == 1){
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-	basic.pause(1000)
-    } else {
-        strip.showRainbow(1, 360)
-	basic.pause(1000)
-    }
-})
-radio.onReceivedValue(function (name, value) {
-	if (name == "pulsante" && value == 1) {
-		recieved = 1
-	}
-})
-''',language="javascript")
+st.code('''''',language="javascript")
 
 e,edit  = st.columns([1,1])
 with edit:
