2b7184bb156f6254d3bc117a2d47786726897e32
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2021-11-19 12:12:48 +0100

    Update iotgo-bz.py

--- webapp/iotgo-bz.py
+++ webapp/iotgo-bz.py
@@ -675,7 +675,7 @@ def genURL (*args):#input_name, output_name):#here i am collecting chunks of cod
 
 
 
-urlis,jscode=genURL([input_name[0],output_name[0]],[input_name[1],output_name[1]])
+urlis,jscode=genURL([input_name[0],output_name[0]])
 
 
 
