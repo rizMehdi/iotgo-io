e695b449cd9a27f4d7896c5e4787c847957cd99b
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-03-29 16:19:12 +0200

    Update iotgopascoli_extended.py

--- webapp/iotgopascoli_extended.py
+++ webapp/iotgopascoli_extended.py
@@ -12,22 +12,24 @@ groupnum="0"
 
 
 
+hide_img_fs = '''
+<style>
+button[title="View fullscreen"]{
+    visibility: hidden;}
+</style>
+'''
 
+fix_sidebar= """
+<style>
+[data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+	width: 350px;}
+[data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+	width: 350px;margin-left: -350px;}
+</style>
+"""
 
-st.markdown(
-        """
-        <style>
-        [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-                width: 350px;
-        }
-        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-                width: 350px;
-                margin-left: -350px;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-)
+st.markdown(hide_img_fs, unsafe_allow_html=True)
+st.markdown(fix_sidebar,unsafe_allow_html=True)
 
 
 
