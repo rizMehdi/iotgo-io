be5785efc36e70f72e646cf91fe17a2bc2c3dd7c
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-03-30 09:23:19 +0200

    Update iotgopascoli_extended.py

--- webapp/iotgopascoli_extended.py
+++ webapp/iotgopascoli_extended.py
@@ -715,6 +715,34 @@ with edit:
 
 	
 htmliframe='''
+
+<script>
+function checkIframeLoaded() {
+    // Get a handle to the iframe element
+    var iframe = document.getElementById('iframe_a');
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+    // Check if loading is complete
+    if (  iframeDoc.readyState  == 'complete' ) {
+        //iframe.contentWindow.alert("Hello");
+        iframe.contentWindow.onload = function(){
+            alert("I am loaded");
+        };
+        // The loading is complete, call the function we want executed once the iframe is loaded
+        afterLoading();
+        return;
+    } 
+
+    // If we are here, it is not loaded. Set things up so we check   the status again in 100 milliseconds
+    window.setTimeout(checkIframeLoaded, 100);
+}
+
+function afterLoading(){
+    alert("I am here");
+}
+</script>
+
+<body onload="checkIframeLoaded();"> 
 <iframe src="
 '''+urlis+'''
 " name="iframe_a" title="Iframe Example" height="1000"  width="700" style="border:none;" scrolling=True></iframe>
