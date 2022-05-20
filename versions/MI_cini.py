9e3a3bef49b79b5055f3efaecce52b58df9c74d6
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-05-20 15:40:44 +0200

    warning seq changed.

--- versions/MI_cini.py
+++ versions/MI_cini.py
@@ -1008,10 +1008,14 @@ if prevUrlis != urlis:
 prevUrlis=urlis
 
 
-if p2ptype=="ricevo dati":
-    st.warning('ricorda che devi cambiare la parola "replace" in tuo codice con quello che ti aspetti di ricevere dai tuoi amici')
+#warnings:
 if input_name[0]=="soilMoistureHigh" or input_name[0]=="soilMoistureLow" or  input_name[1]=="soilMoistureHigh" or input_name[1]=="soilMoistureLow":
     st.warning('ricorda che il sensore di umidità del suolo è esterno. Deve essere fissato fisicamente al micro:bit.')
+if p2ptype=="ricevo dati":
+    st.warning('ricorda che devi cambiare la parola "replace" in tuo codice con quello che ti aspetti di ricevere dai tuoi amici')
+
+
+
 
 e,edit  = st.columns([1,1])
 with edit:
