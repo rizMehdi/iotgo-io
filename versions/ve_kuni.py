f6a3dcb3e31668a453af5be9450af297d5d6e86b
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-05-21 11:56:47 +0200

    author badge

--- versions/ve_kuni.py
+++ versions/ve_kuni.py
@@ -1083,6 +1083,12 @@ st.markdown("""---""")
 st.write("Un progetto di / A project of:")
 st.image("https://raw.githubusercontent.com/IoTgo-app/iotgo-io/main/images/unilogo.png",width=600)
 version="ve_2.0.11"
-st.write("Mehdi Rizvi | IoTgo version "+version)
+st.write("IoTgo version "+version)
 # st.markdown("<h6 style='text-align: right; color: grey;'>By Mehdi Rizvi | "+version+"</h6>", unsafe_allow_html=True)
 
+
+
+authorbadge="""
+[![Mehdi Rizvi](https://img.shields.io/badge/Author-@rizMehdi-grey.svg?colorA=gray&colorB=dodgerblue&logo=github)](https://github.com/rizMehdi/)
+"""
+st.markdown(authorbadge,  unsafe_allow_html=False)
