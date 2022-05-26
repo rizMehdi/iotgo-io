057b6155116fe9aaf48c594fc9920c3ffdb7be76
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-05-26 12:53:58 +0200

    typo fix

--- versions/MI_cini.py
+++ versions/MI_cini.py
@@ -870,7 +870,7 @@ def genURL (*args):#input_name, output_name):#here i am collecting chunks of cod
             if True:#eachIOpair[0] != "noInput" and eachIOpair[1] != "noOuput":
                 if eachIOpair[1] in output_code:
                     if eachIOpair[1]=="sendData":
-                        if p2ptype=='invio dati' && gamelevel==1: #gamelevel==0:
+                        if p2ptype=='invio dati' and gamelevel==1: #gamelevel==0:
                             if_body_code=output_code[eachIOpair[1]].replace("inputName",input_name[1][0:8]).replace("inputValue","1")
 #                             if_body_code=output_code[eachIOpair[1]].replace("inputName",input_name[0][0:8]).replace("inputValue","1")
                         elif p2ptype=='ricevo dati': #gamelevel==1: #NOT NEEDED REALLY
